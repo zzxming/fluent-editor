f510486aacd089678973d4361139e2e28ef98648
zzxming <74341337+zzxming@users.noreply.github.com>
2025-03-03 23:18:22 +0800

    test: fix e2e test (#244)

--- packages/docs/playwright.config.ts
+++ packages/docs/playwright.config.ts
@@ -70,10 +70,11 @@ export default defineConfig({
     // },
   ],
 
+  timeout: 120 * 1000,
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'pnpm dev',
+    port: 5173,
+    reuseExistingServer: !process.env.CI,
+  },
 })
